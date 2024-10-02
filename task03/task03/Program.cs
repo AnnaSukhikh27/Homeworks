aed9da9df6f726f5624a5d779f8db6f1865cca54
Анна Сухих <suhihannie@yandex.ru>
2024-10-02 18:04:12 +0500

    Задание выполнено. Поменять местами сотни и десятки в четырехзначном числе

---
+++ task03/task03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырехзначное число");
+
+            var number = int.Parse(Console.ReadLine());
+
+            var thousands = number / 1000;
+            var hundreds = (number / 100) % 10;
+            var tens = (number / 10) % 10;
+            var units = number % 10;
+
+            var newNumber = thousands * 1000 + hundreds * 10 + tens * 100 + units;
+
+            Console.WriteLine(newNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
