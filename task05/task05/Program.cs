6b04d606a0e23e0ddfad5fdd34295661770b2626
Анна Сухих <suhihannie@yandex.ru>
2024-10-07 19:48:33 +0500

    Задание 5 выполнено. Вариант 15

---
+++ task05/task05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = CountExp(2, 3) + CountExp(5, 7) + CountExp(4, 6);
+
+            Console.WriteLine ($"x = {x:F3}");
+
+            Console.ReadKey();
+        }
+
+        static double CountExp(double a, double b) {
+            return (Math.Exp(a) - Math.Exp(-a)) / (Math.Exp (b) + Math.Exp(-b));
+        }
+    }
+}
