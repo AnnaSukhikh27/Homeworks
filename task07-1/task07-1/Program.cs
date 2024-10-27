c7ffec0f7416ba8de717d63c01e543a99c3ff1fe
Анна Сухих <suhihannie@yandex.ru>
2024-10-27 10:14:49 +0500

    Задание 7-1 выполнено. Вариант 15

---
+++ task07-1/task07-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = GetNumber("k");
+
+            if (IsStatementTrue(k))
+                Console.WriteLine("Число не кратно ни 2, ни 3");
+            else
+                Console.WriteLine("Число кратно или 2, или 3, или обоим числам");
+
+            Console.ReadKey();
+        }
+
+        static bool IsStatementTrue(int k)
+        {
+            return k % 2 != 0 && k % 3 != 0;
+        }
+
+        static int GetNumber(string numberName) {
+            Console.WriteLine($"Введите число {numberName}");
+            return int.Parse(Console.ReadLine());
+        
+        }
+    }
+}
