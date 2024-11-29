f8be0d8b582d7f0e9bd7029fb8c5d603afda7b7b
Анна Сухих <suhihannie@yandex.ru>
2024-11-29 13:12:12 +0500

    Задание 8 выполнено! Вариант 15

---
+++ task08/task08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента функции");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x}) = {MyFunction(x)}");
+
+            Console.ReadKey();
+        }
+        static double MyFunction(double x)
+        {
+            if (x < -1)
+                return (1 - x * x);
+            else if (x > 1)
+                return (x - 1);
+
+            return 0;
+        }
+    }
+}
