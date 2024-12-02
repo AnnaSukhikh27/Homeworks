643c08ff222db8f866bad151120212e8dc02c7b4
Анна Сухих <suhihannie@yandex.ru>
2024-12-02 16:39:38 +0500

    Задание 10.1 выполнено. Вариант 15

---
+++ task10/task10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = 6350;
+            double H;
+            for (double d = 1; d <= 10; d++)
+            {
+                H = Math.Sqrt((R + d) * (R + d) - (R * R));
+
+                Console.WriteLine("При h = " + d + " Расстояние до линии горизонта = " + H);
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
