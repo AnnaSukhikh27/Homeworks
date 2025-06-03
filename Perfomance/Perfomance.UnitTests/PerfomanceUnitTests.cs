9bf6939f597e6e2dbd34631828f8616498c0a380
Анна Сухих <suhihannie@yandex.ru>
2025-06-04 02:03:29 +0500

    Возникли проблемы с кодировкой файла. Исправила. Задание 13 выполнено. Вариант 15

--- Perfomance/Perfomance.UnitTests/PerfomanceUnitTests.cs
+++ Perfomance/Perfomance.UnitTests/PerfomanceUnitTests.cs
@@ -7,7 +7,7 @@ namespace Perfomance.UnitTests
         public void ConstructorTest()
         {
             var romeo = CreateTestPerfomance();
-            Assert.That(romeo.Name, Is.EqualTo("Ромео и Джульетта"));
+            Assert.That(romeo.Name, Is.EqualTo("Р РѕРјРµРѕ Рё Р”Р¶СѓР»СЊРµС‚С‚Р°"));
             Assert.That(romeo.Duration, Is.EqualTo(new TimeSpan(2, 0, 0)));
             Assert.That(romeo.Beginning, Is.EqualTo(new DateTime(2025, 6, 10, 18, 0, 0)));
             Assert.That(romeo.Type, Is.EqualTo(PerfomanceType.Regular));
@@ -18,21 +18,21 @@ namespace Perfomance.UnitTests
         public void GetInfoTest()
         {
             var romeo = CreateTestPerfomance();
-            romeo.Description = "История трагической любви";
+            romeo.Description = "РСЃС‚РѕСЂРёСЏ С‚СЂР°РіРёС‡РµСЃРєРѕР№ Р»СЋР±РІРё";
             romeo.Coefficient = 0.0;
             var info = romeo.GetInfo();
 
             Assert.That(info.Length, Is.EqualTo(4));
-            Assert.That($"Ромео и Джульетта 02:00:00", Is.EqualTo(info[0]));
-            Assert.That(info[1], Is.EqualTo("Описание: История трагической любви"));
-            Assert.That($"Начало: 10.06.2025 18:00:00, Окончание: 10.06.2025 20:00:00", Is.EqualTo(info[2]));
-            Assert.That($"Тип: обычный, Скидка/Увеличение цены: 0", Is.EqualTo(info[3]));
+            Assert.That($"Р РѕРјРµРѕ Рё Р”Р¶СѓР»СЊРµС‚С‚Р° 02:00:00", Is.EqualTo(info[0]));
+            Assert.That(info[1], Is.EqualTo("РћРїРёСЃР°РЅРёРµ: РСЃС‚РѕСЂРёСЏ С‚СЂР°РіРёС‡РµСЃРєРѕР№ Р»СЋР±РІРё"));
+            Assert.That($"РќР°С‡Р°Р»Рѕ: 10.06.2025 18:00:00, РћРєРѕРЅС‡Р°РЅРёРµ: 10.06.2025 20:00:00", Is.EqualTo(info[2]));
+            Assert.That($"РўРёРї: РѕР±С‹С‡РЅС‹Р№, РЎРєРёРґРєР°/РЈРІРµР»РёС‡РµРЅРёРµ С†РµРЅС‹: 0", Is.EqualTo(info[3]));
 
             
         }
         private TheaterPerfomance CreateTestPerfomance()
         {
-            return new TheaterPerfomance("Ромео и Джульетта", new TimeSpan(2, 0, 0), new DateTime(2025, 6, 10, 18, 0, 0), PerfomanceType.Regular);
+            return new TheaterPerfomance("Р РѕРјРµРѕ Рё Р”Р¶СѓР»СЊРµС‚С‚Р°", new TimeSpan(2, 0, 0), new DateTime(2025, 6, 10, 18, 0, 0), PerfomanceType.Regular);
         }
     }
     
